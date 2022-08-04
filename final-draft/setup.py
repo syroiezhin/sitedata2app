e1cb6558b5bce5483cd419bd8ddaa74ee3222581
Valerii Syroiezhin <86792918+syroiezhin@users.noreply.github.com>
2022-08-04 06:23:32 +0300

    python setup.py py2app

--- final-draft/setup.py
+++ final-draft/setup.py
@@ -1,16 +1,26 @@
 from setuptools import setup
-# 
-''' pip install -U -r requirements.txt ''' # !!!TURN YOUR ATTENTION!!!
-# 
+ 
+''' pip install -U -r requirements.txt '''
+
+APP_NAME = 'URL2IP'
 APP = ['tk_wbs_IP.py']
-APP_NAME = 'WbsIPv2'
+
 DATA_FILES = ['wbs_IP.py','icon.png']
-OPTIONS = {'iconfile': 'icon.png'} #.icns
+OPTIONS = {'iconfile': 'icon.png'}
 
 setup(
     app=APP,
     name=APP_NAME,
     data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
     setup_requires=['py2app'],
+    options={'py2app': OPTIONS},
+
+    version='1.0',
+    description='site data search',
+    long_description="Getting IP and information about the site knowing the url address",
+
+    author='@NEU3RON',
+    license="Syroiezhin",
+    author_email='v.syroiezhin@gmail.com',
+    url="https://github.com/syroiezhin",
 )
